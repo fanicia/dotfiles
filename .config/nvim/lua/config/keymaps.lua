5d27052b3afa2ed6f0170cd4d77ed50ea431ad8d
fanicia <mortenzc42@gmail.com>
2025-02-23 10:18:04 +0100

    Setup custom keymap for switching background theme.

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,24 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Toogle between light and dark theme
+
+local wk = require("which-key")
+
+wk.add({
+  -- Make a custom keymap section
+  { "<leader>o", desc = "Custom Stuff", icon = LazyVim.config.icons.kinds.Keyword }, -- group
+  {
+    "<leader>ob",
+    mode = "n",
+    function()
+      if vim.o.background == "light" then
+        vim.o.background = "dark"
+      else
+        vim.o.background = "light"
+      end
+    end,
+    desc = "Toggle Background Theme",
+  },
+})
